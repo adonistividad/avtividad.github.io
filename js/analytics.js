19b9a3e595317f889e82b78cf9b6b4f875ff270d
avtividad <avtividad@yahoo.com>
2016-03-13 07:11:06 +0400

    Added files via upload

---
+++ js/analytics.js
@@ -0,0 +1,18 @@
+﻿(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-64677475-1', 'auto');    //tividad.com
+ga('send', 'pageview');
+
+//ga('create', 'UA-64677475-7', 'auto'); //resumeze.com
+//ga('create', 'UA-64677475-6', 'auto'); //mindspc.com
+//ga('create', 'UA-64677475-5', 'auto');    //kidswaynursery.com
+//ga('create', 'UA-64677475-4', 'auto');    //mauble.com
+//ga('create', 'UA-64677475-3', 'auto');    /tividad.com/kidsway 
+//ga('create', 'UA-64677475-2', 'auto');    //tividad.com/me-af
+//ga('create', 'UA-64677475-1', 'auto');    //tividad.com
+
